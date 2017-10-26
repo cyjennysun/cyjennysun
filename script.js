7d772c1b826147dba3e78fc7513cb1f442cf0ea3
Chi-Ying Sun <Chi-Ying@Chi-Yings-MacBook-Pro.local>
2017-10-25 21:43:01 -0500

    my first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+	$(document).ready( function() {
+  	$('.portfolio-content a').click( function() {
+    	$('.portfolio-content a').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
+
+	
